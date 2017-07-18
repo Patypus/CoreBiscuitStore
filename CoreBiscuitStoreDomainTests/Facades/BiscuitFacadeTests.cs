1ddbec550d07f282f7d28c76156ebd8d986a9d47
patrick howell <patrick.howell@outlook.com>
2017-07-18 21:51:32 +0100

    Use xUnit for testing

---
+++ CoreBiscuitStoreDomainTests/Facades/BiscuitFacadeTests.cs
@@ -0,0 +1,26 @@
+﻿using CoreBiscuitStoreDomain.Facades;
+using CoreBiscuitStoreDomain.Facades.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CoreBiscuitStoreDomainTests.Facades
+{
+    public class BiscuitFacadeTests
+    {
+        private readonly IBiscuitFacade _facade;
+
+        public BiscuitFacadeTests()
+        {
+            _facade = new BiscuitFacade();
+        }
+
+        [Fact]
+        public void GetAllBiscuitTypes_ReturnsExpectedEntities()
+        {
+            var result = _facade.GetAllBiscuitTypes();
+            Assert.False(true);
+        }
+    }
+}
---
+++ CoreBiscuitStoreWebTests/WebUnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CoreBiscuitStoreWebTests
+{
+    public class WebUnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.False(true);
+        }
+    }
+}
