d9b606011c49865c8f8b02a26e85a608df18625c
patrick howell <patrick.howell@outlook.com>
2017-07-19 20:45:34 +0100

    Adding data layer

---
+++ CoreBiscuitStoreData/IRepositoryFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBiscuitStoreData
+{
+    public interface IRepositoryFactory
+    {
+        string Test();
+    }
+}
---
+++ CoreBiscuitStoreData/RepositoryFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBiscuitStoreData
+{
+    public class RepositoryFactory : IRepositoryFactory
+    {
+        public string Test()
+        {
+            return "Test";
+        }
+    }
+}
