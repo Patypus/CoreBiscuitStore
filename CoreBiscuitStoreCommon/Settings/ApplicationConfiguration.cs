66d935523fd8b2065f34b83cfb1af188950a031c
patrick howell <patrick.howell@outlook.com>
2017-07-26 21:50:38 +0100

    Added common projected for shared details

---
+++ CoreBiscuitStoreCommon/Settings/ApplicationConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreBiscuitStoreCommon.Settings
+{
+    public class ApplicationConfiguration
+    {
+        public string Database { get; set; }
+
+        public string ConnectionString { get; set; }
+    }
+}
