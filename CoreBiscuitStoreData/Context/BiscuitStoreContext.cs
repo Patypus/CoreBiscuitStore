761f1284ffcfd484d415e3e493e98489c8a7d5d8
patrick howell <patrick.howell@outlook.com>
2017-08-01 22:12:55 +0100

    Added concrete data context layer

---
+++ CoreBiscuitStoreData/Context/BiscuitStoreContext.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using CoreBiscuitStoreData.Entities;
+using CoreBiscuitStoreCommon.Settings;
+using MongoDB.Bson;
+
+namespace CoreBiscuitStoreData.Context
+{
+    public class BiscuitStoreContext : IBiscuitStoreContext
+    {
+        private IMongoDatabase _database;
+
+        public BiscuitStoreContext(ApplicationConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.ConnectionString);
+
+            if (client != null)
+            {
+                _database = client.GetDatabase(configuration.Database);
+            }
+        }
+
+        public IMongoCollection<Biscuit> Biscuits
+        {
+            get
+            {
+                return _database.GetCollection<Biscuit>("Biscuit");
+            }
+        }
+    }
+}
---
+++ CoreBiscuitStoreData/Context/IBiscuitStoreContext.cs
@@ -0,0 +1,13 @@
+﻿using CoreBiscuitStoreData.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBiscuitStoreData.Context
+{
+    public interface IBiscuitStoreContext
+    {
+        IMongoCollection<Biscuit> Biscuits { get;  }
+    }
+}
