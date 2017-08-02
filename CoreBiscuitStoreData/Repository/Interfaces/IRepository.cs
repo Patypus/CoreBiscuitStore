b17ff8d3c07e827784681690cfe8b932bafb964a
patrick howell <patrick.howell@outlook.com>
2017-08-02 22:07:33 +0100

    Replaced biscuit repository interface with generic typed version

---
+++ CoreBiscuitStoreData/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoreBiscuitStoreData.Repository.Interfaces
+{
+    public interface IRepository<T>
+    {
+        /// <summary>
+        /// Gets all entities of the type the interface has.
+        /// </summary>
+        /// <returns>Task to resolve the enumerable of all entities in the repository</returns>
+        Task<IEnumerable<T>> GetAll();
+    }
+}
