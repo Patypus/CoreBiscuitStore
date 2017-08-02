169eb740864f34bfa481776e75dba05c3ad26138
patrick howell <patrick.howell@outlook.com>
2017-08-02 22:07:12 +0100

    Replaced repository fatory with repository resolver

---
+++ CoreBiscuitStoreData/Repository/Resolver/IRepositoryResolver.cs
@@ -0,0 +1,15 @@
+﻿using CoreBiscuitStoreData.Repository.Interfaces;
+
+namespace CoreBiscuitStoreData.Repository.Resolver
+{
+    public interface IRepositoryResolver
+    {
+        /// <summary>
+        /// Gets the concrete implementation of IRepository for the type passed.
+        /// </summary>
+        /// <typeparam name="T">Entity type to get repository for</typeparam>
+        /// <exception cref="NotFoundException">Thrown if no repository is found for type</exception>
+        /// <returns>Implemetation of a repository for type T</returns>
+        IRepository<T> GetRepository<T>();
+    }
+}
---
+++ CoreBiscuitStoreData/Repository/Resolver/RepositoryResolver.cs
@@ -0,0 +1,23 @@
+﻿using CoreBiscuitStoreCommon.Settings;
+using CoreBiscuitStoreData.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBiscuitStoreData.Repository.Resolver
+{
+    public class RepositoryResolver : IRepositoryResolver
+    {
+        private readonly ApplicationConfiguration _configuration;
+
+        public RepositoryResolver(ApplicationConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IRepository<T> GetRepository<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
