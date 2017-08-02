aa081db312c2079e7021aa972579f91c0db8c9b9
patrick howell <patrick.howell@outlook.com>
2017-08-02 22:06:00 +0100

    added not found exception

---
+++ CoreBiscuitStoreCommon/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CoreBiscuitStoreCommon.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
