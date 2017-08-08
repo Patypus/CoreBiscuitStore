dbb3e55511d09add0fcbe5c3a38a8356fe6be348
patrick howell <patrick.howell@outlook.com>
2017-08-08 21:28:04 +0100

    Remove unit test

--- CoreBiscuitStoreDataTests/Repository/Resolver/RepositoryResolverTests.cs
+++ CoreBiscuitStoreDataTests/Repository/Resolver/RepositoryResolverTests.cs
@@ -26,24 +26,5 @@ namespace CoreBiscuitStoreDataTests.Repository.Resolver
 
             Assert.True(result.GetType() == typeof(BiscuitRepository));
         }
-
-        [Fact]
-        public void AllEntitiesHaveRepositoryImplementation()
-        {
-            var assemblyTypes = typeof(RepositoryResolver).GetTypeInfo().Assembly.GetTypes();
-
-            var entitiyTypes = assemblyTypes.Where(type => type is IEntity);
-
-    //        var thingsThere = entitiyTypes.All(entityType => entityType is IRepository);
-
-    //        var repositoryTypes = assemblyTypes.Where(type => type is IRepository);
-    //        var types = .GetTypes().Where(type => type);
-    //        types.ToList();
-    //        AssemblyLoadContext.GetLoadContext(). GetLoadedAssemblies()  System.Reflection.App AppDomain.CurrentDomain.GetAssemblies()
-    //.SelectMany(s => s.GetTypes())
-    //.Where(p => type.IsAssignableFrom(p));
-
-    //        Assert.True(false);
-        }
     }
 }
